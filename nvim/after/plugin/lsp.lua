b2b0053eb46e71305fa82fe759dd410de153dd90
ztnel <christian911@sympatico.ca>
2024-04-15 21:54:05 -0400

    add gitsigns defaults and readd global lsp keybindings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,7 +5,15 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 lspconfig.pyright.setup{}
+lspconfig.cmake.setup{}
 lspconfig.clangd.setup{}
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
