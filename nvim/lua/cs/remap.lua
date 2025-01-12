ac5be15fd3d1937a2b200e58e8314a2676ba521c
Christian Sargusingh <christian911@sympatico.ca>
2025-01-12 13:42:43 -0500

    Update remap.lua

--- nvim/lua/cs/remap.lua
+++ nvim/lua/cs/remap.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap('i', '<TAB>', 'pumvisible() ? "\\<C-n>" : "\\<TAB>"', { expr = true, silent = true })
 vim.api.nvim_set_keymap('i', '<S-TAB>', 'pumvisible() ? "\\<C-p>" : "\\<C-h>"', { expr = true })
+vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+vim.g.copilot_no_tab_map = true
